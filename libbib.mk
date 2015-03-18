2c6bc05e3d98852a840ca6841b6ca408332f53da
Daniel Bosk <dbosk@kth.se>
2015-03-18 11:12:22 +0100

    Adds a make include file for libbib dependencies

---
+++ libbib.mk
@@ -0,0 +1,14 @@
+LIBBIB+=anon.bib
+LIBBIB+=crypto.bib
+LIBBIB+=meta.bib
+LIBBIB+=otrmsg.bib
+LIBBIB+=ppes.bib
+LIBBIB+=surveillance.bib
+
+LIBBIB+=ac.acr
+LIBBIB+=crypto.acr
+LIBBIB+=stdterm.acr
+LIBBIB+=surveillance.acr
+
+${LIBBIB}: libbib
+	[ -e "./$@" ] || ln -s libbib/$@ ./$@
