fbee26e8f883cfaaff86c6ca3ac109690a19c5a7
Daniel Bosk <dbosk@kth.se>
2015-07-16 10:10:20 +0200

    Adds paper on AC, adds WWW acronyms

--- ac.mk
+++ ac.mk
@@ -6,6 +6,16 @@ AC_FILES+= TowardsPPACwHPHCHD.pdf
 TowardsPPACwHPHCHD.pdf:
 	wget -O $@ http://ieeexplore.ieee.org/ielx5/6287257/6297910/06297915.pdf?tp=&arnumber=6297915&isnumber=6297910
 
+
+.PHONY: AccessControl
+AccessControl: AccessControl.pdf
+	evince $^
+
+AC_FILES+= AccessControl.pdf
+AccessControl.pdf:
+	wget $@ http://ieeexplore.ieee.org/ielx1/35/7577/00312842.pdf?tp=&arnumber=312842&isnumber=7577
+
+
 .PHONY: clean-depends clean-ac
 clean-depends: clean-ac
 clean-ac:
