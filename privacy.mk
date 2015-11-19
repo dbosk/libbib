5813dd389db7454587490bdf88d5742eecd36677
Daniel Bosk <dbosk@kth.se>
2015-11-19 15:33:30 +0100

    Adds ThresholdUserSearch

--- privacy.mk
+++ privacy.mk
@@ -7,6 +7,15 @@ DifferentialPrivacy.pdf:
 	wget -O $@ file:///dev/null
 
 
+.PHONY: ThresholdUserSearch
+ThresholdUserSearch: ThresholdUserSearch.pdf
+	evince $^
+
+PRIVACY_FILES+= ThresholdUserSearch.pdf
+ThresholdUserSearch.pdf:
+	wget -O $@ http://link.springer.com${LIBRARY_EXT}/content/pdf/10.1007%2F978-3-642-55137-6_15.pdf
+
+
 .PHONY: clean-depends clean-privacy
 clean-depends: clean-privacy
 clean-privacy:
