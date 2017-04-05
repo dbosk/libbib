8d9bfc4b9236c4da3c8b6afe5f2c0602b4f470c4
Daniel Bosk <dbosk@kth.se>
2017-04-05 16:07:05 +0200

    Adds refkeys for protests and voting

--- protests.mk
+++ protests.mk
@@ -1,5 +1,8 @@
+BIBSP_REFKEY+=2017WomensMarchesInUS
 URL-2017WomensMarchesInUS=https://www.theatlantic.com/technology/archive/2017/01/womens-march-protest-count/514166/
 
+BIBSP_REFKEY+=2016DemonstrationsInSeoul
 URL-2016DemonstrationsInSeoul=http://bigstory.ap.org/article/317ea62bddbd4132ab1467863a532ab9/counting-1-million-crowds-anti-president-rallies-seoul
 
+BIBSP_REFKEY+=CVCrowdCounting
 URL-CVCrowdCounting=https://dx.doi.org/10.1109/CVPR.2008.4587569
