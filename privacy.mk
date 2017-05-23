06d95e63cabe4b72ea6c7288104ac0c745a7a3af
Daniel Bosk <dbosk@kth.se>
2017-05-23 16:22:47 +0200

    Adds ref for DataTrack

--- privacy.mk
+++ privacy.mk
@@ -1,3 +1,5 @@
 BIBSP_REFKEY+=DifferentialPrivacy
 URL-DifferentialPrivacy=https://www.cis.upenn.edu/~aaroth/Papers/privacybook.pdf
 
+BIBSP_REFKEY+=DataTrack
+URL-DataTrack=http://doi.acm.org/10.1145/2702613.2732701
