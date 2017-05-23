c2b5a61c64f117b0191cbaf8a1f8142c5f7ed06b
Daniel Bosk <dbosk@kth.se>
2017-05-23 16:22:09 +0200

    Moves ThresholdUserSearch download code

--- privacy.mk
+++ privacy.mk
@@ -1,5 +1,3 @@
 BIBSP_REFKEY+=DifferentialPrivacy
 URL-DifferentialPrivacy=https://www.cis.upenn.edu/~aaroth/Papers/privacybook.pdf
 
-BIBSP_REFKEY+=ThresholdUserSearch
-URL-ThresholdUserSearch=http://link.springer.com${LIBRARY_EXT}/content/pdf/10.1007%2F978-3-642-55137-6_15.pdf
