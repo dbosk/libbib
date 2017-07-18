45fb50acfc470fde4882e1243ea427d18b2e99be
Daniel Bosk <dbosk@kth.se>
2017-07-18 12:04:57 +0200

    Restructures bibsp.mk

--- bibsp.mk
+++ bibsp.mk
@@ -1,60 +1,55 @@
-BIBSP_BIB+=ac.bib
-BIBSP_BIB+=anon.bib
-BIBSP_BIB+=auth.bib
-BIBSP_BIB+=be.bib
-BIBSP_BIB+=crypto.bib
-BIBSP_BIB+=depend.bib
-BIBSP_BIB+=location.bib
-BIBSP_BIB+=meta.bib
-BIBSP_BIB+=nfc.bib
-BIBSP_BIB+=osn.bib
-BIBSP_BIB+=otrmsg.bib
-BIBSP_BIB+=otpkx.bib
-BIBSP_BIB+=ppes.bib
-BIBSP_BIB+=privacy.bib
-BIBSP_BIB+=reputation.bib
-BIBSP_BIB+=surveillance.bib
-BIBSP_BIB+=mpc.bib
-BIBSP_BIB+=passwd.bib
-BIBSP_BIB+=protests.bib
-BIBSP_BIB+=voting.bib
-BIBSP_BIB+=stats.bib
-BIBSP_BIB+=adhocnets.bib
-BIBSP_BIB+=hr.bib
+BIBSP_TOPIC+=	auth passwd ac
+BIBSP_TOPIC+=	anon
+BIBSP_TOPIC+=	crypto be mpc
+BIBSP_TOPIC+=	privacy meta surveillance hr
+BIBSP_TOPIC+=	protests voting
+BIBSP_TOPIC+=	osn
+BIBSP_TOPIC+=	reputation
+BIBSP_TOPIC+=	location
+BIBSP_TOPIC+=	stats
+BIBSP_TOPIC+=	blockchain depend
+BIBSP_TOPIC+=	nfc
+BIBSP_TOPIC+=	adhocnets
+
+BIBSP_TOPIC+=	otrmsg otpkx ppes
+
+
+### BIB FILES ###
+
+define bibsp_bibfiles
+BIBSP_BIB+=		$(1).bib
+endef
+
+$(foreach topic,${BIBSP_TOPIC},$(eval $(call bibsp_bibfiles,${topic})))
+
+
+### STY FILES ###
 
 BIBSP_STY+=libbib.sty
 BIBSP_STY+=bibsp.sty
 
-INCLUDE_LIBBIB?=	.
-INCLUDE_BIBSP?=		${INCLUDE_LIBBIB}
+INCLUDE_BIBSP?=		.
+INCLUDE_LIBBIB?=	${INCLUDE_BIBSP}
 
 ${BIBSP_BIB} ${BIBSP_STY}:
 	[ -e "./$@" ] || ln -s ${INCLUDE_BIBSP}/$@ ./$@
 
-.PHONY: clean-depends
-clean-depends: clean-bibsp
+.PHONY: clean-depends distclean
+clean-depends distclean: clean-bibsp
 clean-bibsp:
 	find ${BIBSP_BIB} ${BIBSP_STY} -type l | xargs ${RM}
 
-include ${INCLUDE_BIBSP}/ac.mk
-include ${INCLUDE_BIBSP}/anon.mk
-include ${INCLUDE_BIBSP}/auth.mk
-include ${INCLUDE_BIBSP}/be.mk
-include ${INCLUDE_BIBSP}/crypto.mk
-include ${INCLUDE_BIBSP}/depend.mk
-include ${INCLUDE_BIBSP}/location.mk
-include ${INCLUDE_BIBSP}/meta.mk
-include ${INCLUDE_BIBSP}/osn.mk
-include ${INCLUDE_BIBSP}/ppes.mk
-include ${INCLUDE_BIBSP}/privacy.mk
-include ${INCLUDE_BIBSP}/reputation.mk
-include ${INCLUDE_BIBSP}/mpc.mk
-include ${INCLUDE_BIBSP}/passwd.mk
-include ${INCLUDE_BIBSP}/protests.mk
-include ${INCLUDE_BIBSP}/voting.mk
-include ${INCLUDE_BIBSP}/stats.mk
-include ${INCLUDE_BIBSP}/adhocnets.mk
-include ${INCLUDE_BIBSP}/hr.mk
+
+### INCLUDE FILES ###
+
+define bibsp_includes
+include 		${INCLUDE_BIBSP}/$(1).mk
+endef
+
+$(foreach topic,${BIBSP_TOPIC},$(eval $(call bibsp_includes,${topic})))
+
+
+### OPEN FILES ###
 
 define bibsp_display
 .PHONY: $(1)
