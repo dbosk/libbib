6fe465364c3d79952a56060525685901fedf85f4
Daniel Bosk <dbosk@kth.se>
2017-07-18 12:04:30 +0200

    Adds some blockchain references

---
+++ blockchain.mk
@@ -0,0 +1,5 @@
+BIBSP_REFKEY+=	BlockchainFreeCryptocurrencies
+URL-BlockchainFreeCryptocurrencies=https://eprint.iacr.org/2016/871.pdf
+
+BIBSP_REFKEY+=	FairProofOfWork
+URL-FairProofOfWork=https://eprint.iacr.org/2016/680.pdf
