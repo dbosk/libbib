0f50da7be88848276edaf8bf6a6f53c3e356df1b
Daniel Bosk <dbosk@kth.se>
2017-09-11 14:07:11 +0200

    Refactors bibsp
    - No options except lncs.

--- ac.mk
+++ ac.mk
@@ -1,3 +1,6 @@
+BIBSP_REFKEY+=Bell-LaPadula
+URL-Bell-LaPadula=http://www.dtic.mil/docs/citations/AD0770768
+
 BIBSP_REFKEY+=TowardsPPACwHPHCHD
 URL-TowardsPPACwHPHCHD=http://ieeexplore.ieee.org/ielx5/6287257/6297910/06297915.pdf?tp=&arnumber=6297915&isnumber=6297910
 
