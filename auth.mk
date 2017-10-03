38f9f4542e84b3fdde7e70de28bb7e0d53228589
Daniel Bosk <dbosk@kth.se>
2017-10-03 15:40:16 +0200

    Adds "How to win the Clone Wars"

--- auth.mk
+++ auth.mk
@@ -1,2 +1,5 @@
 BIBSP_REFKEY+=P2PPasswords
 URL-P2PPasswords=http://ieeexplore.ieee.org/ielx5/6329089/6335787/06335797.pdf
+
+BIBSP_REFKEY+=HowToWinTheCloneWars
+URL-HowToWinTheCloneWars=http://doi.acm.org/10.1145/1180405.1180431
