0bab79e97bf415ad059e1b803928de6fc708e98a
Daniel Bosk <dbosk@kth.se>
2017-10-10 10:39:20 +0200

    Adds Psignatures and NIZK acronym

--- auth.mk
+++ auth.mk
@@ -3,3 +3,6 @@ URL-P2PPasswords=http://ieeexplore.ieee.org/ielx5/6329089/6335787/06335797.pdf
 
 BIBSP_REFKEY+=HowToWinTheCloneWars
 URL-HowToWinTheCloneWars=http://doi.acm.org/10.1145/1180405.1180431
+
+BIBSP_REFKEY+=Psignatures
+URL-Psignatures=https://doi.org/10.1007/978-3-540-78524-8_20
