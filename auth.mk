b6231db177ccd6a84493d648ad46eeb70719eb7d
Daniel Bosk <dbosk@kth.se>
2018-02-16 14:56:35 +0100

    Adds distance bounding to auth

--- auth.mk
+++ auth.mk
@@ -6,3 +6,9 @@ URL-HowToWinTheCloneWars=http://doi.acm.org/10.1145/1180405.1180431
 
 BIBSP_REFKEY+=Psignatures
 URL-Psignatures=https://doi.org/10.1007/978-3-540-78524-8_20
+
+BIBSP_REFKEY+=DistanceBounding
+URL-DistanceBounding=https://link.springer.com/chapter/10.1007/3-540-48285-7_30
+
+BIBSP_REFKEY+=DistanceHijacking
+URL-DistanceHijacking=http://ieeexplore.ieee.org/abstract/document/6234408/
