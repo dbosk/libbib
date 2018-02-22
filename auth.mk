293e3f0422995eab503d34b75d95f4f22f538c95
Daniel Bosk <dbosk@kth.se>
2018-02-22 15:17:28 +0100

    Adds CLsignatures URL

--- auth.mk
+++ auth.mk
@@ -1,6 +1,9 @@
 BIBSP_REFKEY+=P2PPasswords
 URL-P2PPasswords=http://ieeexplore.ieee.org/ielx5/6329089/6335787/06335797.pdf
 
+BIBSP_REFKEY+=CLsignatures
+URL-CLsignatures=https://link.springer.com/chapter/10.1007/978-3-540-28628-8_4
+
 BIBSP_REFKEY+=HowToWinTheCloneWars
 URL-HowToWinTheCloneWars=http://doi.acm.org/10.1145/1180405.1180431
 
