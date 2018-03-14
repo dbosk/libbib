897dd6f26ede5bb52a00cce9829209b45bf18483
Daniel Bosk <dbosk@kth.se>
2018-03-14 14:28:46 +0100

    Writes up auth section up to distance bounding

--- auth.mk
+++ auth.mk
@@ -1,27 +1,24 @@
-BIBSP_REFKEY+=MafiaFraud
-URL-MafiaFraud=https://link.springer.com/chapter/10.1007/3-540-48184-2_3
-
 BIBSP_REFKEY+=DistanceBounding
 URL-DistanceBounding=https://link.springer.com/chapter/10.1007/3-540-48285-7_30
 
+BIBSP_REFKEY+=MafiaFraud
+URL-MafiaFraud=https://link.springer.com/chapter/10.1007/3-540-48184-2_3
 BIBSP_REFKEY+=DistanceHijacking
 URL-DistanceHijacking=http://ieeexplore.ieee.org/abstract/document/6234408/
-
+BIBSP_REFKEY+=TerroristFraud
+URL-TerroristFraud=https://scholar.google.fr/scholar?hl=en&as_sdt=0%2C5&q=+%09+Major+Security+Problems+with+the+%22Unforgeable%22+%28Feige%29-Fiat-Shamir+Proofs+of+Identity+and+How+to+Overcome+Them&btnG=
 BIBSP_REFKEY+=ImpersonationFraud
 URL-ImpersonationFraud=https://link.springer.com/chapter/10.1007/978-3-642-04474-8_21
-
 BIBSP_REFKEY+=DB-BMV
 #URL-DB-BMV=https://content.iospress.com/articles/journal-of-computer-security/jcs518
 URL-DB-BMV=https://eprint.iacr.org/2013/465
 
 BIBSP_REFKEY+=DB-DFKO
 URL-DB-DFKO=https://link.springer.com/chapter/10.1007%2F978-3-642-24861-0_4
-
-BIBSP_REFKEY+=DBonSmartphones
-URL-DBonSmartphones=https://hal.inria.fr/hal-01244606
-
 BIBSP_REFKEY+=ProProx
 URL-ProProx=https://link.springer.com/chapter/10.1007/978-3-319-26059-4_6
+BIBSP_REFKEY+=DBonSmartphones
+URL-DBonSmartphones=https://hal.inria.fr/hal-01244606
 BIBSP_REFKEY+=CLsignatures
 URL-CLsignatures=https://link.springer.com/chapter/10.1007/978-3-540-28628-8_4
 
