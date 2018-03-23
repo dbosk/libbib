5cb1757290c857e068616f2eca21dad193571a50
Daniel Bosk <dbosk@kth.se>
2018-03-23 11:07:19 +0100

    Ensures BIBSP_TOPIC is defined before any use

--- bibsp.mk
+++ bibsp.mk
@@ -1,3 +1,6 @@
+ifndef BIBSP_MK
+BIBSP_MK=true
+
 BIBSP_LINK+=  bibsp.sty
 INCLUDE_BIBSP?=.
 BIBSP_TOPIC+=   crypto
@@ -24,6 +27,11 @@ BIBSP_TOPIC+=	nfc
 BIBSP_TOPIC+=	ecurrency
 BIBSP_TOPIC+=	otrmsg otpkx ppes
 BIBSP_TOPIC+=   ac
+define bibsp_bibfiles
+BIBSP_LINK+=		$(1).bib
+endef
+
+$(foreach topic,${BIBSP_TOPIC},$(eval $(call bibsp_bibfiles,${topic})))
 ${BIBSP_LINK}:
 	[ -e "./$@" ] || ln -s ${INCLUDE_BIBSP}/$@ ./$@
 .PHONY: distclean
@@ -37,13 +45,11 @@ $(1):
 endef
 
 $(foreach ref,${BIBSP_REFKEY},$(eval $(call bibsp_display,${ref})))
-define bibsp_bibfiles
-BIBSP_LINK+=		$(1).bib
-endef
-
-$(foreach topic,${BIBSP_TOPIC},$(eval $(call bibsp_bibfiles,${topic})))
+bibsp.sty: $(foreach b,${BIBSP_TOPIC},${b}.bib)
 define bibsp_includes
 include 		$${INCLUDE_BIBSP}/$(1).mk
 endef
 
 $(foreach topic,${BIBSP_TOPIC},$(eval $(call bibsp_includes,${topic})))
+
+endif
