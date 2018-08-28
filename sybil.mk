7d3944ddbc86c4284472106dc881d63a1ce060f6
Daniel Bosk <dbosk@kth.se>
2018-08-28 14:45:38 +0200

    Renames reputation to sybil, adds proof-of-personhood

--- sybil.mk
+++ sybil.mk
@@ -1,2 +1,5 @@
 BIBSP_REFKEY+=SybilAttack
 URL-SybilAttack=http://link.springer.com${LIBRARY_EXT}/content/pdf/10.1007%2F3-540-45748-8_24.pdf
+
+BIBSP_REFKEY+=proof-of-personhood
+URL-proof-of-personhood=https://doi.org/10.1109/EuroSPW.2017.46
