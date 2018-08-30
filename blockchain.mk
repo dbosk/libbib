7270e52c800d0583c8fbcc30c25798ca8a84fafd
Daniel Bosk <dbosk@kth.se>
2018-08-30 18:13:22 +0200

    Adds collective signing (cothority) paper

--- blockchain.mk
+++ blockchain.mk
@@ -4,6 +4,9 @@ URL-Bitcoin= 	https://bitcoin.org/bitcoin.pdf
 BIBSP_REFKEY+= 	OmniLedger
 URL-OmniLedger=https://eprint.iacr.org/2017/406
 
+BIBSP_REFKEY+= 	collective-signing
+URL-collective-signing=https://dx.doi.org/10.1109/SP.2016.38
+
 BIBSP_REFKEY+=	BlockchainFreeCryptocurrencies
 URL-BlockchainFreeCryptocurrencies=https://eprint.iacr.org/2016/871.pdf
 
